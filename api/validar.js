480eca8628888bd37df6780ecc1079be6bae96a1
keef021 <alexandremilas2011@gmail.com>
2025-08-24 15:09:59 -0300

    Update validar.js

--- api/validar.js
+++ api/validar.js
@@ -1,18 +1,15 @@
-import { connectToMongo } from "./mongo.js"
+import Database from "@replit/database";
+
+const db = new Database();
 
 export default async function handler(req,res){
-    const key = req.query.key?.trim()
-    if(!key) return res.status(400).json({ valid:false })
+    const key = req.query.key?.trim();
+    if(!key) return res.status(400).json({ valid:false });
 
-    const { db } = await connectToMongo()
-    const keysCollection = db.collection("keys")
-
-    const agora = new Date()
-    const keyData = await keysCollection.findOne({ key })
-
-    if(keyData && (agora - keyData.createdAt) <= 24*60*60*1000){
-        res.status(200).json({ valid:true })
+    const timestamp = await db.get(key);
+    if(timestamp && (Date.now() - timestamp <= 24*60*60*1000)){
+        res.status(200).json({ valid:true });
     } else {
-        res.status(200).json({ valid:false })
+        res.status(200).json({ valid:false });
     }
 }
